afb172c0bae8cb17c73a5f31cd12a5894884a4ce
Mariel-Kozberg <mkozberg@mgh.harvard.edu>
2024-12-25 16:31:10 -0500

    Update linescan_d.m

--- linescan_d.m
+++ linescan_d.m
@@ -1,20 +1,18 @@
 %% calculate vessel diameter 
+% inputs: raw 2-photon linescan data collected perpendicular to the vessel in order to measure vessel diameter 
+% outputs: indices of edges of blood vessel over timecourse (ind1 = left, ind2 = right) 
 
-% load 
-
+% load data sets 
 close all; clear variables; 
-mouse = 'APP23_4'; 
-session = 2; 
-loaddir = ['/Volumes/mgkdata/APP23/' mouse '_' num2str(session) '/'];
-homedir = '/users/marielkozberg/Documents/code/';
+mouse = ''; % input mouse # 
+session = 1; % select imaging session 
+loaddir = ['']; % select loading directory 
+homedir = ''; % select home directory 
 cd(['/Volumes/mgkdata/APP23/analysis/' mouse '/' mouse '_' num2str(session)]) 
 mkdir(['/Volumes/mgkdata/APP23/analysis/' mouse '/' mouse '_' num2str(session) '_diametersubtr']);
 savedir = ['/Volumes/mgkdata/APP23/analysis/' mouse '/' mouse '_' num2str(session) '_diametersubtr'];
 
-
-% Tg = 1; % 1 Tg, 2 WT 
-
-for v = 18:23
+for v = 1:5; % select which vessels to analyze 
 vessel = v; 
 filename = ['LSD' num2str(vessel) '.oif.files']; %   
 if exist([loaddir filename])
@@ -28,8 +26,8 @@ time = linspace(1/fr,2.5,lines);
 linescan_1 = imread(['s_C001.tif']); % color 1 - methoxy 
 linescan_2 = imread(['s_C002.tif']); % color 2 - FITC 
 
+linescan_2c = linescan_2- linescan_1; % subtract methoxy from FITC channel 
 
-linescan_2c = linescan_2- linescan_1; 
 LS2 = figure; 
 subplot(1,3,1); 
 imagesc(linescan_2);
@@ -37,6 +35,7 @@ subplot(1,3,2);
 imagesc(linescan_1); 
 subplot(1,3,3); 
 imagesc(linescan_2c);  colormap gray; 
+
 % threshold 
 linescan_2z = ones(size(linescan_2c)); 
 for i = 1:size(linescan_2c,1) 
@@ -62,27 +61,6 @@ for i = 1:size(linescan_2t,1)
 [~,ind2(i)] = find(linescan_2t(i,:)==1,1,'last'); 
 end 
 
-
-% calculate valleys for Tg
-%if Tg == 1
-  %  caa = input('Is there CAA around vessel? Reply 1 = yes, 2 = no   '); 
-   % if caa == 1 
-    
-% linescanmean = mean(linescan_2z,1); 
-% d2 = diff(diff(linescanmean)); 
-% [~, indval1] = max(d2(1,1:round(end/2)));
-% [~, indval2t] = max(d2(1,round(end/2):end));
-% indval2 = indval2t +  round(size(d2,2)/2); 
-% corr1 = indval1 - mean(ind1); 
-% corr2 = mean(ind2) - indval2; 
-%savedir = ['/Volumes/mgkdata/APP23/analysis/S191F3/' mouse];
-%cd(savedir); 
-%savefile = ['LSD' num2str(vessel) '_corr'];
-%save(savefile,'corr1','corr2');
- %   end 
-%else 
-% end 
-%close(LS2)
 cd(savedir); 
 
 indplot = figure; plot(ind1.*.3310,'b'); hold on; plot(ind2.*.3310,'b');
@@ -103,6 +81,6 @@ cd(savedir);
 savefile = ['LSD' num2str(vessel)];
 save(savefile,'linescan_2t','ind1','ind2'); 
 else 
-    display(['no file ' num2str(v)]);
+    display(['no file ' num2str(v)]); 
 end 
 end
